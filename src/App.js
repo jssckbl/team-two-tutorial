44fef5901d03a837fcf3eb84f6243eea5999a2e5
yorkmac036 <jacob.ukkelberg@gmail.com>
2021-12-22 15:09:09 -0600

    h2 add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Team Two Tutorial Github Sandbox</h1>
         <p>Create new components, create content, and practice all the github things.</p>
     </div>
+      <h2>Jacob's line</h2>
   );
 }
 
