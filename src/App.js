6d69800b0627b2dc2d8e0f87a25b1135a2993668
cmoua051 <cmoua051@gmail.com>
2021-12-22 15:19:22 -0600

    Practice commit/push

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Team Two Tutorial Github Sandbox</h1>
         <p>Create new components, create content, and practice all the github things.</p>
     </div>
+      <h2>Monkeys for $1000 each</h2>
   );
 }
 
