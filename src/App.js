5cf128f5199ef4f5607f627dbd30840670f8eec6
AdamKasper <adamgkasper@gmail.com>
2021-12-22 15:16:32 -0600

    button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <h1>Team Two Tutorial Github Sandbox</h1>
         <p>Create new components, create content, and practice all the github things.</p>
     </div>
+      <div>
+          <button>Click me!</button>
+      </div>
   );
 }
 
