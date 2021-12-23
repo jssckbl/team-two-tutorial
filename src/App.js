210cb771e5d0cb8335bf6cd91a3757f3d3afdf9e
yorkmac016 <yansen.hadiwikarsa@gmail.com>
2021-12-23 09:49:06 -0600

    sen branch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Team Two Tutorial Github Sandbox</h1>
         <p>Create new components, create content, and practice all the github things.</p>
         <h2>EXOTIC ANIMALS FOR SALE!!</h2>
+      <p>Sen stuff</p>
     </div>
   );
 }
